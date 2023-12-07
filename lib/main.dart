c605ebbd4f3724c9a748e270d3168e6de1e4c799
maurodev16 <mauro.developer.br@gmail.com>
2023-12-07 12:38:57 +0100

    ios integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -17,7 +16,7 @@ import 'pages/Authentication/Pages/LoginPage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
   await GetStorage.init();
   await initializeDateFormatting();
